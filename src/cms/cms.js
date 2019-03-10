d5053e8adb6ec6a805507a71ef37bb261f71b80f
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-03-10 15:23:59 +0900

    move static

---
+++ src/cms/cms.js
@@ -0,0 +1,8 @@
+import CMS from "netlify-cms"
+
+// import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import BlogPostPreview from "./preview-templates/BlogPostPreview"
+// import ProductPagePreview from './preview-templates/ProductPagePreview'
+
+// CMS.registerPreviewStyle('/styles.css')
+// CMS.registerPreviewTemplate("blog", BlogPostPreview)
